9f98d2ea0d52890ba119cb9539dae79df1789972
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-02-15 14:06:25 -0300

    primeiro commit

---
+++ src/Pages/Ohlala/index.jsx
@@ -0,0 +1,12 @@
+import { Main } from './style'
+
+function Ohlala() {
+    return (
+        <>
+            <Main>
+                
+            </Main>
+        </>
+    )
+}
+export default Ohlala;
